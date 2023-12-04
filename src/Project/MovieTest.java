338fdecfeec2a59b21a467312fa534a03f0c9b73
minhyeok Bang <bmh89895656@gmail.com>
2023-12-04 15:27:28 +0900

    2023 12 04 1527

---
+++ src/Project/MovieTest.java
@@ -0,0 +1,13 @@
+package Project;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieTest {
+	public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+		MovieMenu mm = new MovieMenu();
+		mm.movieMenu();
+
+
+
+	}
+}
